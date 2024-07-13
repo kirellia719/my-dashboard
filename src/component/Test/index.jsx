3244c6ac447e05dd450c9b19b5059b5223941b4f
Khanh <kirellia719@gmail.com>
2024-07-13 11:36:07 +0700

    xoa gg map api

--- src/component/Test/index.jsx
+++ src/component/Test/index.jsx
@@ -1,59 +1,54 @@
-// src/components/Map.js
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import customIconUrl from "leaflet/dist/images/marker-icon.png";
-import customIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
-import customIconShadowUrl from "leaflet/dist/images/marker-shadow.png";
 
-// Fix for marker icons not showing correctly
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-   iconRetinaUrl: customIconRetinaUrl,
-   iconUrl: customIconUrl,
-   shadowUrl: customIconShadowUrl,
-});
-
-const LocationMarker = () => {
-   const [position, setPosition] = useState(null);
-   const map = useMap();
-
-   useEffect(() => {
-      if (navigator.geolocation) {
-         const watchId = navigator.geolocation.watchPosition((pos) => {
-            console.log(pos);
-            const { latitude, longitude } = pos.coords;
-            const newPosition = [latitude, longitude];
-            setPosition(newPosition);
-            map.setView(newPosition, map.getZoom());
-         });
-
-         return () => {
-            navigator.geolocation.clearWatch(watchId);
-         };
-      }
-   }, [map]);
-
-   return position === null ? null : (
-      <Marker position={position}>
-         <Popup>You are here</Popup>
-      </Marker>
+const MapPlaceholder = () => {
+   return (
+      <div
+         style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "#f0f0f0",
+         }}
+      >
+         <p>Loading map...</p>
+      </div>
    );
 };
 
-const Map = () => {
-   const initialPosition = [51.505, -0.09];
+const CustomMap = () => {
+   const [isMapReady, setIsMapReady] = useState(false);
+
+   // Fake a loading state for demo purposes
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         setIsMapReady(true);
+      }, 2000); // Simulate a 2 seconds loading time
+
+      return () => clearTimeout(timer);
+   }, []);
 
    return (
-      <MapContainer center={initialPosition} zoom={13} style={{ height: "100vh", width: "100vw" }}>
-         <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-         />
-         <LocationMarker />
-      </MapContainer>
+      <>
+         {!isMapReady && <MapPlaceholder />}
+         {isMapReady && (
+            <MapContainer
+               center={[51.505, -0.09]}
+               zoom={13}
+               style={{ height: "100vh", width: "100%" }}
+               whenReady={() => setIsMapReady(true)}
+            >
+               <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+               />
+            </MapContainer>
+         )}
+      </>
    );
 };
 
-export default Map;
+export default CustomMap;
